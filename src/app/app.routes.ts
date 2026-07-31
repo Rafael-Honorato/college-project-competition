4982b58b727138dbdea7c55ed67c35549cf79a86
Rafael Honorato <contatorafaelhonorato@gmail.com>
2026-07-31 13:47:01 -0300

    feat: add all routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,4 +11,44 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./pages/home/home.component').then((m) => m.HomeComponent),
   },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./pages/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent,
+      ),
+  },
+  {
+    path: 'projects',
+    loadComponent: () =>
+      import('./pages/projects/projects.component').then(
+        (m) => m.ProjectsComponent,
+      ),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register/register.component').then(
+        (m) => m.RegisterComponent,
+      ),
+  },
+  {
+    path: 'students',
+    loadComponent: () =>
+      import('./pages/students/students.component').then(
+        (m) => m.StudentsComponent,
+      ),
+  },
+  {
+    path: 'submit-project',
+    loadComponent: () =>
+      import('./pages/submit-project/submit-project.component').then(
+        (m) => m.SubmitProjectComponent,
+      ),
+  },
 ];
