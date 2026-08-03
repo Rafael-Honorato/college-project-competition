532cc11780482df052668157d616211417b118aa
Rafael Honorato <contatorafaelhonorato@gmail.com>
2026-08-03 11:46:47 -0300

    remveção do menu

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
