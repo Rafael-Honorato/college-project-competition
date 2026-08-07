623070eecd500305324b6c20111f82dadc1afacd
Rafael Honorato <contatorafaelhonorato@gmail.com>
2026-08-07 18:13:41 -0300

    refac: Navegação transferiada para componente exclusivo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
     redirectTo: 'home',
+    pathMatch: 'full',
   },
   {
     path: 'home',
