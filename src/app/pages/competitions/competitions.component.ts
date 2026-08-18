31c28ebeeb5aa1a4ec3537271bf285e0b7646a8f
Rafael Honorato <contatorafaelhonorato@gmail.com>
2026-08-18 18:11:35 -0300

    feat: adicao de pipe para status

--- src/app/pages/competitions/competitions.component.ts
+++ src/app/pages/competitions/competitions.component.ts
@@ -2,13 +2,14 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CompetitionsService } from '../../core/services/competitions.service';
 import { Competitions } from '../../core/interfaces/competitions';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute, Router } from '@angular/router';
-import { of, shareReplay } from 'rxjs';
+import { Router } from '@angular/router';
 import { WidtComponent } from './widgets/widt/widt.component';
+import { COMPETITION_STATUS } from '../../core/constants/competitions';
+import { StatusCompetitionPipe } from '../../core/pipes/status-competition';
 
 @Component({
   selector: 'app-competitions',
-  imports: [CommonModule, WidtComponent],
+  imports: [CommonModule, WidtComponent, StatusCompetitionPipe],
   templateUrl: './competitions.component.html',
   styleUrl: './competitions.component.css',
   providers: [CompetitionsService],
@@ -20,5 +21,12 @@ export class CompetitionsComponent {
 
   onEdit(id: number) {
     this.router.navigate(['competition/detail', id]);
+  }
+
+  statusLabel(status: keyof typeof COMPETITION_STATUS | string): string {
+    return (
+      COMPETITION_STATUS[status as keyof typeof COMPETITION_STATUS] ??
+      'Status desconhecido'
+    );
   }
 }
